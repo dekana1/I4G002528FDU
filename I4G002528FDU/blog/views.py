ea6c21903904277e408d26d771417a1d2a230164
Dekana <chinenyeudechukwuu@gmail.com>
2022-06-25 06:23:09 +0100

    Created Templates, Views, Updated urls.py

--- I4G002528FDU/blog/views.py
+++ I4G002528FDU/blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from .models import Post
+from django.views.generic.edit import CreateView, UpdateView, BaseDetailView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog: all')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog: all')
+
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog: all')
